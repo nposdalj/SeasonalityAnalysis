ef34b451f36fe765f26cd71917e86fdc87df9142
nposdalj <36055755+nposdalj@users.noreply.github.com>
2021-07-01 16:34:34 -0700

    Added code for sea surface height anomaly

--- HabMod_GAM_SiteModels.R
+++ HabMod_GAM_SiteModels.R
@@ -1,25 +1,38 @@
 #load libraries
+library(ncdf4)
 
 #define the lat and long of interest
 
 #loading sperm whale data
 site = 'Wake'
-saveDir = paste("G:/My Drive/CentralPac_TPWS_metadataReduced/Wake/Seasonality/")#setting the directory
+saveDir = paste("D:/My Drive/CentralPac_TPWS_metadataReduced/Wake/Seasonality/")#setting the directory
 #load data from StatisicalAnalysis_All
 filenameStatAll = paste(saveDir,site,"_GroupedDay.csv",sep="")
 GroupedDay = read.csv(filenameStatAll) #load files as data frame
 
-
+#load sex specific data
 
 #loading the environmental data
-envDir = paste("G:/My Drive/Gaia_EnvironmentalData/CentralPac/")#setting the directory
+envDir = paste("D:/My Drive/Gaia_EnvironmentalData/CentralPac/")#setting the directory
+#chlorophyll data
 filenameStatAll = paste(envDir,"Chlorophyll.csv",sep="")#load files as data frame
 chl = read.csv(filenameStatAll)
 #subset the dataframe based on the area of interest
 #average the environmental variable based on the ITS over the area of interest
   #save standard deviation 
 
+#SSH anomaly data
+filenameStatAll = paste(envDir,"SSHAnomaly.nc",sep="")#load files as data frame
+SSH = nc_open(filenameStatAll)
+names(SSH$var)
+v1=SSH$var[[1]]
+SSHvar=ncvar_get(SSH,v1)
+SSH_lon=v1$dim[[1]]$vals
+SSH_lat=v1$dim[[2]]$vals
 
+#subset the dataframe based on the area of interest
+#average the environmental variable based on the ITS over the area of interest
+#save standard deviation 
 
 
 
